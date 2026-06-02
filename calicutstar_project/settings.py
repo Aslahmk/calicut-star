3481fb4747ccda77cc7015879aecf00f9d5a26ce
Ahammed Aslah <mkaslah3@gmail.com>
2026-06-03 01:00:59 +0530

    Fix static files with WhiteNoise

--- calicutstar_project/settings.py
+++ calicutstar_project/settings.py
@@ -121,10 +121,17 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MIDDLEWARE = [
+   
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    # keep the rest of your middleware
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
